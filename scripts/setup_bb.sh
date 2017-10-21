6402e0393e3887ff419a7c8fa22637d97d23f16e
gijs.sijpesteijn <gijs@jpoint.nl>
2017-10-21 21:24:43 +0200

    Initial import

--- scripts/setup_bb.sh
+++ scripts/setup_bb.sh
@@ -4,9 +4,20 @@ echo Update ubuntu
 apt-get update
 apt-get -y upgrade
 
+# Format sd card
+# fdisk /dev/mmcblk0
+# delete all partitions
+# press 'n' accept all defaults
+# press 'w'
+# mkfs.ext4 /dev/mmcblk0
+
+parted /dev/mmcblk0 -s "mklabel GPT"
+parted /dev/mmcblk0 -s "mkpart primary 0% 100%"
+mkfs.ext2 /dev/mmcblk0p1
+
 echo Mounting sd card
 mkdir /media/card
-sudo mount -t ext4 -o rw /dev/mmcblk0p2 /media/card
+sudo mount -t ext4 -o rw /dev/mmcblk0p1 /media/card
 
 echo Moving stuff to sdcard
 rm -rf /home
@@ -14,17 +25,25 @@ rm -rf /root
 rm -rf /var/cache
 
 ln -s /media/card/notouch/home /home
+mount --bind /media/card/notouch/home /home
 ln -s /media/card/notouch/root /root
+mount --bind /media/card/notouch/root /root
 ln -s /media/card/notouch/cache /var/cache
+mount --bind /media/card/notouch/var/cache /var/cache
 
+blkid
 ls -l /dev/disk/by-uuid
 vi /etc/fstab
-UUID=b7da6e16-c9cb-4d0c-b90d-9c1c9b5badcc /media/card ext4 defaults,rw,auto,user,exec 0 0
+UUID=b7da6e16-c9cb-4d0c-b90d-9c1c9b5badcc /media/card ext2 defaults,rw,auto,user,exec 0 0
 
 echo Installing opencv
 
+git clone https://github.com/opencv/opencv.git
 apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+cd opencv
+mkdir build
+cd build
 cmake -DWITH_OPENCL=NO -DWITH_OPENCL_SVM=NO -DWITH_OPENCLAMDFFT=NO -DWITH_OPENCLAMDBLAS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
 make
 make install
@@ -32,6 +51,7 @@ ldconfig
 
 echo Installing restbed
 
+git clone https://github.com/Corvusoft/restbed.git
 apt-get -y install libssl-dev
 git submodule init
 git submodule update
